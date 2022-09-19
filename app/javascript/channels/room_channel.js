71b3cd6e4c00250271f3443058fdcd4ab4be6e08
tarakish <akr.3838soccer14@gmail.com>
2022-09-20 00:21:59 +0900

    Feat: チャット機能の実装

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,8 +1,15 @@
 import consumer from "./consumer"
 
 consumer.subscriptions.create("RoomChannel", {
+  // Enter押下時にspeak呼び出し
   connected() {
-    // Called when the subscription is ready for use on the server
+    document.querySelector('input[data-behavior="room_speaker"]').addEventListener('keypress', (event) => {
+      if (event.key === 'Enter') {
+        this.speak(event.target.value);
+        event.target.value = '';
+        return event.preventDefault();
+      }
+    });
   },
 
   disconnected() {
@@ -10,7 +17,8 @@ consumer.subscriptions.create("RoomChannel", {
   },
 
   received(data) {
-    alert(data['message']);
+    const element = document.querySelector('#messages')
+    element.insertAdjacentHTML('beforeend', data['message'])
   },
 
   speak: function(message) {
