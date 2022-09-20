a309195384940c19761317cf313c377595817e86
tarakish <akr.3838soccer14@gmail.com>
2022-09-21 00:54:56 +0900

    Feat: 認証機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,7 @@
 class RoomsController < ApplicationController
   def show
+    # 仮コード
+    cookies.signed[:user_id] = User.first.id
     @messages = Message.all
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_19_133634) do
+ActiveRecord::Schema.define(version: 2022_09_20_115717) do
 
   create_table "messages", force: :cascade do |t|
     t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
