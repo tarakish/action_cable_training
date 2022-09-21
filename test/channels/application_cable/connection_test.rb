e103006fdc6ae82475b91dcab3208c3e93aa8352
tarakish <akr.3838soccer14@gmail.com>
2022-09-21 19:43:26 +0900

    Feat: Action Cableのテストを追記

--- test/channels/application_cable/connection_test.rb
+++ test/channels/application_cable/connection_test.rb
@@ -1,11 +1,15 @@
 require "test_helper"
 
 class ApplicationCable::ConnectionTest < ActionCable::Connection::TestCase
-  # test "connects with cookies" do
-  #   cookies.signed[:user_id] = 42
-  #
-  #   connect
-  #
-  #   assert_equal connection.user_id, "42"
-  # end
+  test "connection accepts" do
+    user = User.first
+    cookies.signed[:user_id] = user.id
+    connect
+    assert_equal connection.current_user.id, user.id# connectionで接続したConnectionオブジェクト取得
+  end
+
+  test "connection rejects" do
+    cookies.signed[:user_id] = nil
+    assert_reject_connection { connect }
+  end
 end
--- test/channels/room_channel_test.rb
+++ test/channels/room_channel_test.rb
@@ -1,8 +1,21 @@
 require "test_helper"
 
 class RoomChannelTest < ActionCable::Channel::TestCase
-  # test "subscribes" do
-  #   subscribe
-  #   assert subscription.confirmed?
-  # end
+  test "subscribes" do
+    subscribe
+    assert subscription.confirmed?
+    assert_has_stream "room_channel"
+  end
+
+  test 'broadcast' do
+    subscribe
+    text = "hello"
+    broadcast_text = ApplicationController.render(
+      partial: 'messages/message',
+      locals: { message: Message.new(content: text) }
+    )
+    assert_broadcast_on('room_channel', message: broadcast_text) do
+      perform :speak, message: text
+    end
+  end
 end
